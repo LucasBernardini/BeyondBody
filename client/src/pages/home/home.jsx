0b35eeb045275e3778dd14cb0eee90cddc95cfd0
Lucas Bernardini <lucas.bernardini97@gmail.com>
2020-07-16 14:51:25 -0400

    completion of home page mobile styling, import fonts and create4 button animation/mixin

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
             <div className="home__body">
             <p>Hello, Welcome to Beyond Body, a journalling experience that allows you to express your emotions and thoughts to improve your overall mental health.</p>
             </div>
-            <button className="home__button">Press to Continue</button>
+            <button className="home__button"><span>Continue</span></button>
         </div>
     )
 }
