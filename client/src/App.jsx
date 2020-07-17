22bb4ac2e77ef57c143f61b744071b4e1e3e819b
Lucas Bernardini <lucas.bernardini97@gmail.com>
2020-07-16 23:47:26 -0400

    firebase functionality added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,66 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import './App.css';
 import Navbar from './navbar/navbar'
 import Home from './pages/home/home';
+import Login from './pages/login/login';
+import Journal from './pages/journal/journal';
 
-function App() {
+firebase.initializeApp({
+  apiKey: "AIzaSyBvmnNBQp3DSf4rKhI5d4b9qqMTqwS34WQ",
+  authDomain: "beyond-body.firebaseapp.com"
+})
+
+class App extends React.Component {
+  state = { isSignedIn: false };
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }  
+
+  componentDidMount = () =>{
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({isSignedIn:!!user})
+    })
+  }
+
+  render(){
   return (
     <div className="App">
+      {this.state.isSignedIn ? (
+        <Journal />
+
+      ) : (
+          <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} />
+      )}
     <Navbar />
-    <Home />
-
-
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route>
+          <Login path="/login"/>
+        </Route>
+    </Switch>
+    </Router>
     </div>
   );
+}
 }
 
 export default App;
--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import './home.scss'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 export default function Home() {
     return (
@@ -7,7 +13,9 @@ export default function Home() {
             <div className="home__body">
             <p>Hello, Welcome to Beyond Body, a journalling experience that allows you to express your emotions and thoughts to improve your overall mental health.</p>
             </div>
+            <Link to="/login">
             <button className="home__button"><span>Continue</span></button>
+            </Link>
         </div>
     )
 }
