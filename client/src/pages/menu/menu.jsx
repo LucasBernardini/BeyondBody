b670432d9aadd907fe9f76c6546a6aba0ab41629
Lucas Bernardini <lucas.bernardini97@gmail.com>
2020-07-21 00:21:38 -0400

    fetch database information to display in menu

--- client/src/pages/menu/menu.jsx
+++ client/src/pages/menu/menu.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import firebase from '../../firebase';
 
-export default function menu() {
+export default function Menu() {
+    const [entries, setEntries] = React.useState([]);
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+            const db = firebase.firestore()
+            const data = await db.collection("Lucas-Bernardini").get()
+            setEntries(data.docs.map(doc => doc.data()))
+        }
+        fetchData()
+    }, [])
+
     return (
-        <div>
-            
-        </div>
+        <ul>
+            {entries.map(entry => (
+                <li>{entry.name}</li>
+            ))}
+        </ul>
     )
 }
