adb6e3f6ebca339333c0d7e1ddcf79951688126d
Lucas Bernardini <lucas.bernardini97@gmail.com>
2020-07-26 16:21:18 -0400

    fix the footer positioning & button layout

--- client/src/navbar/navbar.jsx
+++ client/src/navbar/navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './navbar.scss'
 import firebase from "firebase";
-import { BrowserRouter as Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Link, Redirect, useHistory } from "react-router-dom";
 
 export default function Navbar(props) {
+    const history = useHistory();
+
     return (
         <div className="style">
             {props.isSignedIn ? (
@@ -12,7 +14,7 @@ export default function Navbar(props) {
         <h1 className="navbar__header">Beyond <br />Body</h1>
         </div>
         <div>
-        <button className="navbar__button" onClick={() => firebase.auth().signOut().then(() => <Redirect exact to="/" />)} >Signout</button>
+        <button className="navbar__button" onClick={() => history.push("/")} >Signout</button>
         </div>
           </div>
         ) : (
--- client/src/pages/footer/footer.jsx
+++ client/src/pages/footer/footer.jsx
@@ -7,12 +7,14 @@ import './footer.scss'
 export default function Footer() {
     return (
         <div className="footer">
+            <div className="footer__container">
             <div className="footer__container--1">
                 <Link exact to="/"className="footer__direct">Home</Link>
                 <Link to="/about"className="footer__direct">about</Link>
             </div>
             <div className="footer__container--2">
                 <p>Are you seeking help? Visit Canadas <a className="footer__help" href="https://www.canada.ca/en/public-health/services/mental-health-services/mental-health-get-help.html">Mental Health support page</a></p>
+            </div>
             </div>
         </div>
     )
