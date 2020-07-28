409e427cbccfe95d52ed5b4a80eda538779b736e
Lucas Bernardini <lucas.bernardini97@gmail.com>
2020-07-28 00:35:19 -0400

    type fix

--- src/pages/footer/footer.jsx
+++ src/pages/footer/footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
             <div className="footer__container">
             <div className="footer__container--1">
                 <Link exact to="/"className="footer__direct">Home</Link>
-                <Link to="/about"className="footer__direct">about</Link>
+                <Link to="/about"className="footer__direct">About</Link>
             </div>
             <div className="footer__container--2">
                 <p>Are you seeking help? Visit Canadas <a className="footer__help" href="https://www.canada.ca/en/public-health/services/mental-health-services/mental-health-get-help.html">Mental Health support page</a></p>
